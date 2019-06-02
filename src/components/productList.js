1ec8c3037a98022cb07e23a5b6318d6431d40589
jolson615 <jolson615@gmail.com>
2019-06-02 12:24:51 -0400

    v1 solution

--- src/components/productList.js
+++ src/components/productList.js
@@ -3,14 +3,17 @@ import './productList.css'
 import Product from './product'
 
 const ProductList = (props) => {
+  // console.log(props.foods.map(food => ))
   return (
     <div className="ProductList">
-      <Product name={props.foods[0]}
-        price={props.priceList["Apple"]}
-        addToShoppingCart={props.addToShoppingCart}
-        removeFromShoppingCart={props.removeFromShoppingCart}
-      />
-      <Product/>
+      {props.foods.map(food =>
+        <Product key={food}
+          name={food}
+          price={props.priceList[food]}
+          addToShoppingCart={props.addToShoppingCart}
+          removeFromShoppingCart={props.removeFromShoppingCart}
+        />
+      )}
     </div>
   );
 }
